2bf160a6c9fafe63fb578ac44c1dda5c1d14c8c1
koroket <bmasakazu@gmail.com>
2014-12-12 13:06:09 -0800

    databases setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212194523) do
+ActiveRecord::Schema.define(version: 20141212205236) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "link"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
