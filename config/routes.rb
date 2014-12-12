5e7d08f50fb08a45fca533ab5ced9f3672ae272d
koroket <bmasakazu@gmail.com>
2014-12-12 12:58:52 -0800

    fixing database

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'static_pages/profile'
 
   get 'static_pages/addpost'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
