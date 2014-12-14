6a7120bc1783c6c006d0a4b923386c986b198bfc
koroket <bmasakazu@gmail.com>
2014-12-14 15:50:12 -0800

    drop down workin

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  
+  get 'signup'  => 'users#new'
 
   resources :users
 
