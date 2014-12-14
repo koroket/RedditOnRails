a9109594405c937280b1164d7457ccfbbef2430a
Jordan Arnesen <jordan101112@gmail.com>
2014-12-14 14:13:32 -0800

    Add Bootstrap; Start stylesheet; Create user index, header, footer, shim

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
-      @user = User.new
+    @user = User.new
   end
   
   def create
-      flash[:success] = "Welcome to the Sample App!"
-      @user = User.new(user_params)
-      if @user.save
-          redirect_to @user
-          else
-          render 'new'
-      end
+    flash[:success] = "Welcome to the Sample App!"
+    @user = User.new(user_params)
+    if @user.save
+        redirect_to @user
+        else
+        render 'new'
+    end
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
 
   get 'static_pages/signup'
 
@@ -7,6 +6,14 @@ Rails.application.routes.draw do
 
   get 'static_pages/addpost'
   
+  root                'static_pages#home'
+  # get    'help'    => 'static_pages#help'
+  # get    'about'   => 'static_pages#about'
+  # get    'contact' => 'static_pages#contact'
+  # get    's'  => 'users#index'
+  # get    'login'   => 'sessions#new'
+  # post   'login'   => 'sessions#create'
+  # delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
