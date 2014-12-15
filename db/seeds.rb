11f3780b2571463b5b276fe524b633fe680e483b
Jordan Arnesen <jordan101112@gmail.com>
2014-12-14 19:35:44 -0800

    Seed users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             # admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+	title = "Title-#{n+1}"
+	link = "http://www.google.com"
+  body = Faker::Lorem.sentence(5)
+  users.each { |user| user.posts.create!(title: title, link: link, body: body) }
+end
