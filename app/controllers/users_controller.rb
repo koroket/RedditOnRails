1aa39f0e80ec44fe901959cb69d72dfe2c7f2393
Jordan Arnesen <jordan101112@gmail.com>
2014-12-15 11:28:21 -0800

    Add links to all areas of site; Format pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
     redirect_to root_url and return unless @user.activated
   end
 
