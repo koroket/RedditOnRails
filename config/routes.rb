874be8acdb28cc1f2c575a08d95f0ed4f436870f
Jordan Arnesen <jordan101112@gmail.com>
2014-12-16 09:42:42 -0800

    Add ability to edit user info

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,7 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-
   resources :posts
-
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
