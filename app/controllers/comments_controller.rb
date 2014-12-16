7c2efcee2fc56c2c48fe52cc22fb6a18d119e76f
koroket <bmasakazu@gmail.com>
2014-12-15 16:26:37 -0800

    deletion of comments - checks user - masa

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 	  before_action :logged_in_user, only: [:create, :destroy]
-
+	  before_action :correct_user,   only: :destroy
 
   # def index
   #   @posts = Post.all
@@ -45,8 +45,9 @@ class CommentsController < ApplicationController
       params.require(:comment).permit(:content)
     end
     
-    # def correct_user
-    #   @comment = current_user.posts.comments.find_by(id: params[:id])
-    #   redirect_to @post if @comment.nil?
-    # end
+    def correct_user
+      @post = Post.find(session[:current_post_id])
+      @comment = @post.comments.find_by(id: params[:id])
+      redirect_to @post if @comment.nil?
+    end
 end
