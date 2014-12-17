7b7de095b8fdd8578e00efb1e62e72fafe3975bd
Jordan Arnesen <jordan101112@gmail.com>
2014-12-17 10:40:37 -0800

    Fix migrations

--- db/migrate/20141212205236_create_posts.rb
+++ db/migrate/20141212205236_create_posts.rb
@@ -7,6 +7,6 @@ class CreatePosts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index :posts, [:user_id, :created_at]
+    # add_index :posts, [:user_id, :created_at]
   end
 end
