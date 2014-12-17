618e6b2f7e22a5150f2b783e457265a9d9e933ff
koroket <bmasakazu@gmail.com>
2014-12-17 08:19:03 -0800

    adding form validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :body, presence: true
+  validates :subreddit_id, presence: true
 end
