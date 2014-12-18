8e12cd195c2779b1d111d459c6bc41333edcf7c6
Jordan Arnesen <jordan101112@gmail.com>
2014-12-17 21:49:33 -0800

    Implement sort_score updating; display posts in order of sort_score

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jquery-rails',         '4.0.0.beta2'
 gem 'turbolinks',           '2.3.0'
 gem 'jbuilder',             '2.2.3'
 gem 'sdoc',                 '0.4.0', group: :doc
+gem 'time_diff'
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :subreddit
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(sort_score: :desc) }
   validates :user_id, presence: true
   validates :body, presence: true
   validates :subreddit_id, presence: true
@@ -12,7 +12,8 @@ class Post < ActiveRecord::Base
   	upvotes = self.votes.where(isUpvote: true).count
   	downvotes = self.votes.where(isUpvote: false).count
   	votes = upvotes - downvotes
-  	age = Time.now - self.created_at
+  	# age = Time.now - self.created_at
+  	age = Time.diff(Time.now, self.created_at)[:hour]
   	gravity = 1.8
   	new_score = (votes - 1) / (age + 2) ** gravity
 		self.update_attribute(:sort_score, new_score)
